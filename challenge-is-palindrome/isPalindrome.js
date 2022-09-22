5ba4b163cb5777ed103e4c14e76bf74de0494dec
Quinten Krauth <quintenkrauth19@gmail.com>
2022-09-22 09:40:15 -0400

    warm up

---
+++ challenge-is-palindrome/isPalindrome.js
@@ -0,0 +1,14 @@
+
+// Return true if this word is a palindrome. false if it is not. A palindrome is a word that is spelled the same backwards and forwards.
+// For example:
+
+isPalindrom("a")
+// returns true
+
+isPalindrom("noon")
+// returns true
+
+isPalindrom("hello")
+// returns false
+
+// Write your code below
